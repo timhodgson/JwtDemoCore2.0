5177baedbb36e0aaf0ae5e63da62496a252b209c
Tim H <timhodgson@hotmail.com>
2018-07-15 11:31:35 -0400

    1. Consolidate shared code to reduce # of projects 2. Create Ex.JwtTester project 3. Create Ex.WebAPI project 4.  Reduce dependencies on Jwt.Issuer project 5.  Move shared code to Jwt.Shared

---
+++ Ex.Jwt.Tester/Program.cs
@@ -0,0 +1,178 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex.Jwt.Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ex.Jwt.Tester");
+            Console.WriteLine("Sliding Demo");
+            string jwt = SlidingDemo();
+
+            Console.WriteLine(GetValue(jwt, "jadds4z@1688.com").Result);
+
+            //             if (env.IsDevelopment())
+            {
+                Console.WriteLine($"{Environment.NewLine}Ready, press key to close");
+                Console.ReadKey();
+            }
+        }
+
+        /// <summary>
+        /// Demonstrates how to renew the JWT
+        /// </summary>
+        /// <returns></returns>
+        private static string SlidingDemo()
+        {
+            try
+            {
+                var jwt = Login("hrworker@xyz.com", "password").Result;
+                if ( jwt != null )
+                {
+                    Console.WriteLine("Logged in");
+                    Task.Delay(1000).Wait();
+                    var renewed = RenewJwt(jwt).Result;
+                    if (renewed != null)
+                    {
+                        Console.WriteLine("JWT renewed");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Renewal failed");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Login failed");
+                }
+                return jwt;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            return null;
+        }
+
+        private static async Task<String> GetValue(String jwt, String email)
+        {
+            var url = "http://localhost:41932";
+            var apiUrl = $"/api/values/{WebUtility.UrlEncode(email)}";
+
+            using (var client = new HttpClient() { BaseAddress = new Uri(url) })
+            {
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {jwt}");
+
+                using (var response = await client.GetAsync(apiUrl))
+                {
+                    if (response.StatusCode == HttpStatusCode.OK)
+                        return await response.Content.ReadAsStringAsync();
+
+                    else return null;
+                }
+            }
+        }
+
+        #region Login Functions
+        #region RenewJWT
+        /// <summary>
+        /// Renews a JWT, simply by specifying it.
+        /// </summary>
+        /// <param name="jwt">The JWT</param>
+        /// <returns></returns>
+        private static async Task<String> RenewJwt(String jwt)
+        {
+            var url = "";
+            var apiUrl = "";
+#if DEBUG            
+            url = "http://localhost:49842/"; // Ex.Jwt.Issuer
+            apiUrl = "/api/security/renewtoken/";
+#else
+            url = "http://localhost/";
+            apiUrl = "/Jwt.Issuer/api/security/renewtoken/";
+#endif
+
+            using (var client = new HttpClient() { BaseAddress = new Uri(url) })
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                using (var content = new FormUrlEncodedContent(new Dictionary<String, String>() { { "", jwt } }))
+                {
+                    using (var response = await client.PostAsync(apiUrl, content))
+                    {
+                        var result = await response.Content.ReadAsStringAsync();
+
+                        return response.StatusCode == HttpStatusCode.OK ? result : null;
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Login
+        /// <summary>
+        /// Login to the Ex.Jwt.Issuer WebAPI
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        private static async Task<String> Login(String email, String password)
+        {
+            var url = "";
+            var apiUrl = "";
+#if DEBUG            
+            url = "http://localhost:49842/"; // Jwt.Issuer project!
+            apiUrl = "/api/security/login/";
+#else
+            url = "http://localhost/";
+            apiUrl = "/Jwt.Issuer/api/security/login/";
+#endif
+
+            using (var client = new HttpClient() { BaseAddress = new Uri(url) })
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var loginResource = new
+                {
+                    Email = email,
+                    Password = password
+                };
+
+                var resourceDocument = JsonConvert.SerializeObject(loginResource);
+
+                using (var content = new StringContent(resourceDocument, Encoding.UTF8, "application/json"))
+                {
+                    using (var response = await client.PostAsync(apiUrl, content))
+                    {
+                        if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                        {
+                            var result = await response.Content.ReadAsStringAsync();
+
+                            return result;
+                        }
+                        else
+                        {
+                            throw new Exception(string.Format("Uri={2}, Status Code={0}, Error={1}", response.StatusCode, response.ReasonPhrase,
+                                response.RequestMessage.RequestUri.AbsoluteUri));
+                        }
+
+                    }
+                }
+            }
+        }
+        #endregion
+        #endregion
+    }
+
+}
---
+++ Ex.WebAPI/Startup.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using System.Config;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace Ex.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // setup JWT Token validation
+            services.Configure<JwtTokenValidationSettings>(Configuration.GetSection(nameof(JwtTokenValidationSettings)));
+            services.AddSingleton<IJwtTokenValidationSettings, JwtTokenValidationSettingsFactory>();
+            // get environment
+            var env = services.BuildServiceProvider().GetRequiredService<IHostingEnvironment>();
+
+            // Get Database connection config
+            var connectionString = Configuration.GetConnectionString("DbConnection");
+
+            // Get Driver config
+            var databaseDriver = Configuration.GetConnectionString("DatabaseDriver");
+
+            // Setup Database Service layer used in ResourceService
+/*
+            if (databaseDriver.EqualsEx("MySql"))
+                services.AddDbContext<EntityContext>(options => options.UseMySql(connectionString));
+
+            else if (databaseDriver.EqualsEx("InMemory"))
+                services.AddDbContext<EntityContext>(options =>
+                       options.UseInMemoryDatabase("EmployeesDemo")
+               // Suppress not supported Transaction Exceptions
+               .ConfigureWarnings(w => w.Ignore(Microsoft.EntityFrameworkCore.Diagnostics.InMemoryEventId.TransactionIgnoredWarning)));
+
+            else
+                // Fall back to SqlServer
+                services.AddDbContext<EntityContext>(options => options.UseSqlServer(connectionString));
+*/
+
+/*
+            // Setup ResourceService
+            services.AddTransient<ICustomerResourceService, EmployeeResourceService>();
+*/
+            // Setup IIdentity used for simple audit trail in ICustomerResourceService 
+            services.AddTransient<IIdentity, IdentityResolverService>();
+
+            // setup JWT Token validation
+            services.Configure<JwtTokenValidationSettings>(Configuration.GetSection(nameof(JwtTokenValidationSettings)));
+            services.AddSingleton<IJwtTokenValidationSettings, JwtTokenValidationSettingsFactory>();
+
+            // Register the Swagger generator with JWT support
+            services.AddSwaggerGen(c =>
+            {
+                // Tweak for JWT support
+                c.AddSecurityDefinition("Bearer", new Swashbuckle.AspNetCore.Swagger.ApiKeyScheme()
+                {
+                    Description = "Authorization format : Bearer {token}",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
+
+                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info { Title = "Ex.WebApi", Version = "v1" });
+            });
+
+            // Create TokenValidation factory with DI principle
+            var tokenValidationSettings = services.BuildServiceProvider().GetService<IJwtTokenValidationSettings>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+              .AddJwtBearer(options =>
+              {
+                  options.TokenValidationParameters = tokenValidationSettings.CreateTokenValidationParameters();
+                  options.SaveToken = true;
+                  // todo options.RequireHttpsMetadata = false;
+              });
+
+
+            // some samples (this section must contain all the authorization policies used anywhere in the application)
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("HR Only", policy => policy.RequireRole("HR-Worker"));
+                options.AddPolicy("HR-Manager Only", policy => policy.RequireClaim("CeoApproval", "true"));
+            });
+
+            // Secure all controllers by default
+            var authorizePolicy = new AuthorizationPolicyBuilder()
+                                  .RequireAuthenticatedUser()
+                                  .Build();
+
+            // Add Mvc with options
+            services.AddMvc(config => { config.Filters.Add(new AuthorizeFilter(authorizePolicy)); }
+            )
+            // Override default camelCase style (yes its strange the default configuration results in camel case)
+            .AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver();
+                options.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseAuthentication();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Create TokenValidation factory with DI principle
+            var tokenValidationSettings = app.ApplicationServices.GetService<IJwtTokenValidationSettings>();
+
+            // Enable middle ware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middle ware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                //c.ShowRequestHeaders();
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ex.WebApi v1");
+            });
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
